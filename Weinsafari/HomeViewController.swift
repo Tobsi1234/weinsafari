a66f065a4bb2b97d5a56c7c8e0aba01400f7d227
Tobias Steinbrück <tobias.steinbrueck@web.de>
2023-08-31 08:36:20 +0200

    Updated for 2023

--- Weinsafari/HomeViewController.swift
+++ Weinsafari/HomeViewController.swift
@@ -18,14 +18,14 @@ class HomeViewController: UIViewController {
             if granted {
                 let content = UNMutableNotificationContent()
                 content.title = "Erinnerung"
-                content.body = "Die Wein-Safari 2022 beginnt morgen!"
+                content.body = "Die Wein-Safari 2023 beginnt morgen!"
                 content.categoryIdentifier = "alarm"
                 content.sound = UNNotificationSound.default
                 //let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
                 let dateFormatter = DateFormatter()
                 dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
                 dateFormatter.timeZone = TimeZone.current //(abbreviation: "GMT+0:00")
-                let date = dateFormatter.date(from: "2022-09-16 11:30:00")
+                let date = dateFormatter.date(from: "2023-09-15 11:30:00")
 
                 let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date!)
                 let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
@@ -33,7 +33,7 @@ class HomeViewController: UIViewController {
                 //let date1 = Date()
                 //print(date1)
 
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                let request = UNNotificationRequest(identifier: "WeinSafari2023", content: content, trigger: trigger)
                 center.add(request)
                 print("Erinnerung gesetzt.")
             } else {
